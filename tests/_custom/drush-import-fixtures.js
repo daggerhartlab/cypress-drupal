443b94ca735a6c6234d08280017528d84d57fd7d
Jonathan Daggerhart <jonathan@daggerhartlab.com>
2022-09-13 12:56:51 -0400

    Cypress config with pliugin

---
+++ tests/_custom/drush-import-fixtures.js
@@ -0,0 +1,50 @@
+const { spawn } = require('node:child_process');
+
+const CreateUser = function(user) {
+  console.log('DOING: drush user:create role--administrator --mail="role--administrator@example.org" --password="pass"');
+  const userCreate = spawn('lando', [
+    'drush',
+    'user:create',
+    user.username,
+    `--mail="${user.mail}"`,
+    `--password="${user.pass}"`,
+  ], {cwd: '../'})
+
+  userCreate.stdout.on('data', (data) => {
+    console.log(data);
+  })
+  userCreate.stderr.on('data', (data) => {
+    //console.error(`stderr: ${data}`);
+  })
+  userCreate.on('close', (code) => {
+    if (code) {
+      console.log(`-- user:create process exited with code ${code}`);
+    }
+
+    console.log('DOING: drush user:role:add "administrator" role-administrator');
+    user.roles.forEach((role) => {
+      const addRole = spawn('lando', [
+        'drush',
+        'user:role:add',
+        role,
+        user.username,
+      ], {cwd: '../'})
+
+      addRole.stdout.on('data', (data) => {
+        console.log(data);
+      })
+      addRole.stderr.on('data', (data) => {
+        //console.error(`stderr: ${data}`);
+      })
+      addRole.on('close', (code) => {
+        if (code) {
+          console.log(`-- user:role:add process exited with code ${code}`);
+        }
+      });
+    })
+  });
+}
+
+module.exports = {
+  CreateUser
+}
--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,9 +1,35 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
+const {spawn} = require("node:child_process");
+
+/*
+ * Override some config options with a custom json file.
+ */
+let localConfig = {}
+if (fs.existsSync(`${__dirname}/cypress.config.local.json`)) {
+  localConfig = JSON.parse(fs.readFileSync(`${__dirname}/cypress.config.local.json`))
+}
+
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // https://docs.cypress.io/api/plugins/before-run-api
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+
+        /**
+         * Example interaction with drupal through drush.
+         */
+        if (fs.existsSync(`${__dirname}/cypress/fixtures/users/role--administrator.json`)) {
+          let administrator = JSON.parse(fs.readFileSync(`${__dirname}/cypress/fixtures/users/role--administrator.json`))
+          const {CreateUser} = require(`${__dirname}/_custom/drush-import-fixtures.js`);
+
+          CreateUser(administrator);
+        }
+
+
+      })
     },
   },
 });
